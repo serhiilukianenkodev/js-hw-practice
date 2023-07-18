2e375e7ae7be15b2a1fa4d696523b314f44cf483
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2023-07-18 23:09:39 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const itemsMarkup = ingredients.map((item) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = item;
+  return itemEl;
+});
+
+ingredientsEl.append(...itemsMarkup);
