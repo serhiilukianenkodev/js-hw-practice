4e01c5716e2d4a68fd9ec55ba053197dcf6fdd14
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2023-07-23 23:33:04 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,42 @@
+const refs = {
+  inputBloksAmount: document.querySelector("#controls > input"),
+  createBtn: document.querySelector("button[data-create]"),
+  destroyBtn: document.querySelector("button[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+let bloksAmount = 0;
+
+refs.createBtn.addEventListener("click", onCreateBtnClick);
+refs.destroyBtn.addEventListener("click", onDestroyBtnClick);
+
+function onCreateBtnClick() {
+  const createAmount = Number(refs.inputBloksAmount.value);
+  createBoxes(createAmount);
+}
+
+function onDestroyBtnClick() {
+  bloksAmount = 0;
+  refs.boxes.innerHTML = "";
+}
+
+function createBoxes(amount) {
+  let boxesMarkup = "";
+
+  for (let index = 0; index < amount; index++) {
+    const element = `<div style="
+    width: ${30 + bloksAmount * 10 + index * 10}px;
+    height: ${30 + bloksAmount * 10 + index * 10}px;
+    background-color: ${getRandomHexColor()};
+    "></div>`;
+
+    boxesMarkup += element;
+  }
+  bloksAmount += amount;
+
+  refs.boxes.insertAdjacentHTML("beforeend", boxesMarkup);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
