bcd798c5b7cbf4855a7f12c789674e0a09353d8c
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2023-07-23 21:47:44 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const { email, password } = evt.currentTarget.elements;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+
+  if (!emailValue || !passwordValue) {
+    alert("Fill all fields, please");
+    return;
+  }
+
+  const formData = { email: emailValue, password: passwordValue };
+  evt.currentTarget.reset();
+
+  console.log("formData", formData);
+}
